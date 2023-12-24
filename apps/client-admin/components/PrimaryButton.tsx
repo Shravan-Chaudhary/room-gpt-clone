3172a5ed3c389f2c1f812f70d0aab0a34d1b2e83
Shravan <shravansinghchaudhary@gmail.com>
2023-12-24 19:14:18 +0530

    feat(client-admin): update primarybutton to accept custom classnames

--- apps/client-admin/components/PrimaryButton.tsx
+++ apps/client-admin/components/PrimaryButton.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { cn } from "@/lib/utils";
 
-const PrimaryButton = ({ children }: { children: string }) => {
+interface PrimaryButtonProps {
+  children: string;
+  className?: string;
+}
+
+const PrimaryButton = ({ children, className }: PrimaryButtonProps) => {
   return (
-    <>
-      <button className="bg-blue-500 text-white p-4 justify-center flex rounded-lg hover:bg-blue-600">
-        {children}
-      </button>
-    </>
+    <button className={cn("bg-blue-500 text-white p-4 justify-center flex rounded-lg hover:bg-blue-600", className)}>
+      {children}
+    </button>
   );
 };
 
